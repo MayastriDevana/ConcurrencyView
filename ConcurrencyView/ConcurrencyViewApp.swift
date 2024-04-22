1f1092f68b50ee077b525858529212ea42c4c093
Maya <Mayastridevana0205@gmail.com>
2024-04-22 08:45:13 +0700

    Initial Commit

---
+++ ConcurrencyView/ConcurrencyViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyViewApp.swift
+//  ConcurrencyView
+//
+//  Created by User on 22/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
